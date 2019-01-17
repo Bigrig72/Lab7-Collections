7d48392d2634cda1844a09866459b8f97696fc17
Regan Dufort <regan.t.dufort@gmail.com>
2019-01-16 17:22:15 -0800

    getting cards to display on console

---
+++ collections/Classes/Deck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace collections.Classes
+{
+    public class Deck<T> : IEnumerable<T>
+    {
+        T[] DeckOfCards = new T[52];
+        int currentIndex = 0;
+
+        public void Add(T item)
+        {
+            if (currentIndex > DeckOfCards.Length - 1)
+            {
+                Array.Resize(ref DeckOfCards, DeckOfCards.Length + 1);
+            }
+            DeckOfCards[currentIndex] = item;
+            currentIndex++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < currentIndex; i++)
+            {
+                yield return DeckOfCards[i];
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+    
---
+++ collections/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using collections.Classes;
+
+namespace collections
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Deck<Card> Deck = new Deck<Card>();
+            Card card1 = new Card(Rank.Ace,Suit.Clubs);
+            Card card2 = new Card(Rank.Two, Suit.Hearts);
+            Card card3 = new Card(Rank.Three, Suit.Spades);
+            Card card4 = new Card(Rank.Four, Suit.Clubs);
+            Card card5 = new Card(Rank.Five, Suit.Hearts);
+            Card card6 = new Card(Rank.Six, Suit.Diamonds);
+            Card card7 = new Card(Rank.Seven, Suit.Hearts);
+            Card card8 = new Card(Rank.Eight, Suit.Spades);
+            Card card9 = new Card(Rank.Nine, Suit.Diamonds);
+            Card card10 = new Card(Rank.Ten, Suit.Diamonds);
+
+            Deck.Add(card1);
+            Deck.Add(card2);
+            Deck.Add(card3);
+            Deck.Add(card4);
+            Deck.Add(card5);
+            Deck.Add(card6);
+            Deck.Add(card7);
+            Deck.Add(card8);
+            Deck.Add(card9);
+            Deck.Add(card10);
+
+            foreach (Card item in Deck)
+            {
+                Console.WriteLine($"Your card is: {item.Value} of {item.Suit}");
+            }
+        }
+
+
+    }
+}
